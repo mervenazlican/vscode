9e57daf7faad8b736ffc725b7a1e6bba95e88f34
Merve Nazlıcan <merve.nazlican.1@gmail.com>
2022-11-10 13:02:41 +0300

    app.js oluşturuldu

---
+++ app.js
@@ -0,0 +1,19 @@
+const http= require('http')
+const express = require('express')
+const app= express();
+const serveR = http.createServer(app);
+const fs=require('fs');
+
+const dir = "./json"
+serveR.listen(3000)
+
+app.use("/data",(req,res,next) => {
+    fs.readFile(dir +"/turkiye.json",(err,json) => {
+        if(err){
+            res.send("Okunmadı")
+        }else{
+            res.send(JSON.parse(json))
+        }
+    })
+    
+})
